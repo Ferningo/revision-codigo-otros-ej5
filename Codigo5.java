4af741004ff3aa0f36b9325b967166b18bda0f3f
Jose <jose.mar.123@hotmail.com>
2023-02-17 16:04:25 -0600

    Fix bugs
    Corregi errorers de sintaxys y comillas, importe la clase scanner,
    convertid a entero la entrada del usuario.
    
    Borre variables que no se estaban utilizando, al final no entendi bien
    que se supone que tenia que hacer el codigo, los nombres de las
    variables me confundieron.

--- Codigo5.java
+++ Codigo5.java
@@ -1,29 +1,37 @@
+
+import java.util.Scanner;
+
 public class Codigo5 {
 
-	    Scanner s = new Scanner();
-	    System.out.print("Introduzca un número: ');
-	    String ni = s.nextLine();
-	    int c = ni;
-	    
-	    int afo = 0;
-	    int noAfo = 0;
-	    
-	    while (ni > 0) {
-		  int digito = (int)(ni % 10);
-	      if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
-			afo++;
-	      } else {
-			noAfo++;
-		  
-		  ni /= 10;
-	    }
+	public static void main(String[] args) {
 
-	    if (afo > noAfo) {
-	      System.out.prinln("El " + c + " es un número afortunado.");
-	    } else {
-	      System.out.println("El " + c + " no es un número afortunado.");
-	    }
-	    
-	  }
-	  
+		Scanner s = new Scanner(System.in);
+		System.out.print("Introduzca un número: ");
+		int ni = Integer.parseInt(s.nextLine());
+
+		int afo = 0;
+		int noAfo = 0;
+
+		while (ni > 0) {
+			int digito = (int) (ni % 10);
+			if ((digito == 3) || (digito == 7) || (digito == 8) || (digito == 9)) {
+				afo++;
+			} else {
+				noAfo++;
+
+			}
+
+			ni = ni /= 10;
+
+			if (afo > noAfo) {
+				System.out.println("El " + ni + " es un número afortunado.");
+
+			} else {
+				System.out.println("El " + ni + " no es un número afortunado.");
+			}
+
+		}
+
 	}
+
+}
